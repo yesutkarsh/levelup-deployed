050e8e174c163723fe324e4fee316eaf0a5d005b
yesutkarsh <contactutkarshverma@gmail.com>
2025-02-06 21:45:36 +0530

    typescript error fixed and pushed again

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const PremiumLanding = () => {
 
   const { authCard, toggleAuthCard } = useContext(AuthToggle);
   // Using a premium black and white image
-  const bgImageUrl: string = "https://www.masaischool.com/blog/content/images/2023/03/Scholarship-distribution-3.JPG";
+  const bgImageUrl = "https://www.masaischool.com/blog/content/images/2023/03/Scholarship-distribution-3.JPG";
 
   useEffect(() => {
     const img = new Image();
